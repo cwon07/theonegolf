d7a591917ba5493a7e89465cc846453fa04e6965
charlie lin <charlie.pingyi.lin@gmail.com>
2025-03-13 19:56:25 -0700

    more bug fix on date showing at current event

--- src/app/api/current_event/route.ts
+++ src/app/api/current_event/route.ts
@@ -10,11 +10,11 @@ export async function GET(req: Request) {
   try {
     await connectToDatabase();
 
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1); 
 
     // Fetch events with a single query and lean optimization
-    const events = await Event.find({ date: { $gte: today.toISOString().split("T")[0] } }).lean();
+    const events = await Event.find({ date: { $gte: yesterday.toISOString().split("T")[0] } }).lean();
 
     if (events.length === 0) {
       return NextResponse.json({ error: "No upcoming events found" }, { status: 404 });
